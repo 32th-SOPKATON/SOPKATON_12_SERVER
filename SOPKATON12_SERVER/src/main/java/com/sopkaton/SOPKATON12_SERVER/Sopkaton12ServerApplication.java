b0291095c0b5406991e7fd0532f7e67ece5e3e50
jiyeon <sjy6736@naver.com>
2023-05-19 21:09:06 +0900

    [INIT] 초기 프로젝트 세팅

---
+++ SOPKATON12_SERVER/src/main/java/com/sopkaton/SOPKATON12_SERVER/Sopkaton12ServerApplication.java
@@ -0,0 +1,13 @@
+package com.sopkaton.SOPKATON12_SERVER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sopkaton12ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sopkaton12ServerApplication.class, args);
+	}
+
+}
---
+++ SOPKATON12_SERVER/src/test/java/com/sopkaton/SOPKATON12_SERVER/Sopkaton12ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopkaton.SOPKATON12_SERVER;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sopkaton12ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
