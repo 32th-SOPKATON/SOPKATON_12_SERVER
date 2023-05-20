d43c6661d71e2cf33390806903035c029fc9b1d8
jiyeon <sjy6736@naver.com>
2023-05-21 01:53:10 +0900

    #2 [FEAT] 엔티티 구현

---
+++ SOPKATON12_SERVER/src/main/java/com/sopkaton/SOPKATON12_SERVER/domain/Mission.java
@@ -0,0 +1,21 @@
+package com.sopkaton.SOPKATON12_SERVER.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Mission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false)
+    private String content;
+}
+
---
+++ SOPKATON12_SERVER/src/main/java/com/sopkaton/SOPKATON12_SERVER/domain/State.java
@@ -0,0 +1,5 @@
+package com.sopkaton.SOPKATON12_SERVER.domain;
+
+public enum State {
+    BEFORE, DOING, DONE
+}
